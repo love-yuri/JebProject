a71f5763e87b9be3d35d453a557993dc31af78c2
love-yuri <yuri2078170658@gmail.com>
2024-06-06 23:18:32 +0800

    feat: 添加五个新模块

---
+++ androidProject/AbilityTest/app/src/main/java/com/example/abilitytest/fragment/QuestionsFragment.kt
@@ -0,0 +1,25 @@
+package com.example.abilitytest.activity
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.abilitytest.R
+
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [QuestionsFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class QuestionsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_questions, container, false)
+    }
+}

261cab48a93f54ce2962f4bfcf505ca2d120a24a
love-yuri <yuri2078170658@gmail.com>
2024-06-10 18:46:44 +0800

    feat: 重构代码结构，优化项目文件路径常量注释

--- androidProject/AbilityTest/app/src/main/java/com/example/abilitytest/constant.kt
+++ androidProject/AbilityTest/app/src/main/java/com/example/abilitytest/constant.kt
@@ -1,15 +1,13 @@
 package com.example.abilitytest
 
-import android.content.Context
-
 object DBNAME {
-    const val USER = "user.db"
-    const val NOTE = "note.db"
+    const val USER = "user.db" // 默认用户数据库名称
+    const val NOTE = "note.db" // 默认笔记数据库名称
 }
 object FILEPATH {
-    const val AVATAR = "avatar"
-    const val QUESTION_JSON_FILE = "question.json"
-    const val KNOWLEDGE_JSON_FILE = "knowledge.json"
-    const val IMAGE_URI = "file:///android_asset/images/"
-    const val VIDEO_URI = "file:///android_asset/videos/"
+    const val AVATAR = "avatar" // 内部头像存储目录
+    const val QUESTION_JSON_FILE = "question.json" // 问题json文件名
+    const val KNOWLEDGE_JSON_FILE = "knowledge.json" // 知识点json文件名
+    const val IMAGE_URI = "file:///android_asset/images/" // 静态图片默认位置
+    const val VIDEO_URI = "file:///android_asset/videos/" // 静态视频默认位置
 }
